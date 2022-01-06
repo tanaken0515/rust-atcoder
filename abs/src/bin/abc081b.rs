dd78b9acc7ce8cf7c7774b96afb8476b748a924b
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2022-01-07 00:16:24 +0900

    abc081b

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,10 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+    // println!("{}", a.iter().reduce(|accum, item| &(accum | item)).unwrap().trailing_zeros());
+    println!("{}", a.iter().map(|item| item.trailing_zeros()).min().unwrap());
 }
