f91b027b24c8f6360c41873520f8519ffc9bfebc
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2022-01-08 21:37:43 +0900

    abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+        x: u32,
+    }
+
+    let mut ans: u32 = 0;
+    let available500: u32 = if x / 500 > a { a } else { x / 500 };
+    let mut available100: u32;
+
+    for n in 0..=available500 {
+        available100 = if (x - 500 * n) / 100 > b { b } else { (x - 500 * n) / 100 };
+
+        for m in 0..=available100 {
+            if x - 500 * n - 100 * m <= 50 * c {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
