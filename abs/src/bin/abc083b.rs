93eb7f22a09193bd132cd8e9c6f2663be299e71d
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2022-01-09 22:31:29 +0900

    abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        a: u32,
+        b: u32,
+    }
+
+    let mut ans: u32 = 0;
+    for x in 1..=n {
+        let s: u32 = x.to_string().chars().map(|c| c as u32 - 48).sum();
+        if s >= a && s <= b {
+            ans += x;
+        }
+    }
+
+    println!("{}", ans);
 }
